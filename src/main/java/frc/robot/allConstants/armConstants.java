6961becc635330dadfe33c9fca445ec6458ec8f5
Seth Setse <seth@sethsetse.com>
2024-11-10 13:06:25 -0500

    added an arm constants file

---
+++ src/main/java/frc/robot/allConstants/armConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.allConstants;
+
+public class armConstants {
+    //motor id
+    public static final int armMotorID = 11;
+    //motor follower id
+    public static final int armMotorFollowerID = 12;
+    //motor kp, ki, kd
+    public static final double armKP = 0.1, armKI = 0, armKD = 0;
+    //default desired arm value
+    public static final double armDefaultDesiredValue = 0.25;
+    //default base arm value
+    public static final double armDefaultBaseValue = 0;
+    //gear ratio
+    public static final double armGearRatio = 1;
+}
