08c00ae50ec152d99942b7cd605fcf5f4edcd1ba
joelamare <joelamare71@gmail.com>
2024-11-10 19:07:43 -0500

    Claw Commit for idk

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.ClawConstants;
 import static frc.robot.Constants.ClawConstants.*;
+import static frc.robot.subsystems.swervedrive.*;
 
 
 public class ClawSubsystem extends SubsystemBase
