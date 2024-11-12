84554a2c55de9935d6a69c6195e14c2726d4e739
joelamare <joelamare71@gmail.com>
2024-11-12 15:46:43 -0500

    Claw Commit for idk

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -77,8 +76,7 @@ public class ClawSubsystem extends SubsystemBase
         // This method will be called once per scheduler run
     }
 
-
-    @Override
+   @Override
     public void simulationPeriodic()
     {
         // This method will be called once per scheduler run during simulation
