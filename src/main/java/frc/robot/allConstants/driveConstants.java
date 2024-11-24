51c48111b72a78a6cb1f7da8e13df6f50a0439f6
tinbiteabie <tinbite724@gmail.com>
2024-11-24 15:23:40 -0500

    fixed placeholders

--- src/main/java/frc/robot/allConstants/driveConstants.java
+++ src/main/java/frc/robot/allConstants/driveConstants.java
@@ -11,8 +11,8 @@ public final class driveConstants {
     public static final double WHEEL_CIRCUMFERENCE = 2 * WHEEL_RADIUS * Math.PI; // placeholder
     // idk if we'll need these ^^^
     //location of the wheels relative to the center of the robot
-    public static final double moduleDistanceX = 0.381; // placeholder
-    public static final double moduleDistanceY = 0.381; // placeholder
+    public static final double moduleDistanceX = 0.3495; //
+    public static final double moduleDistanceY = 0.324; //
     public static final double moduleDistanceDiagonal=Math.sqrt(Math.pow(moduleDistanceX,2)+Math.pow(moduleDistanceY,2));
 
     //PID calculator values for swerve
@@ -35,22 +35,22 @@ public final class driveConstants {
     public static final double swerveFeedForwardTurnKa = 1; // placeholder
 
     //8 motor ids, 4 encoder ids
-    public static final int driveMotor1 = 5; // placeholder
-    public static final int turnMotor1 = 6; // placeholder
+    //front left
+    public static final int driveMotor1 = 5; //
+    public static final int turnMotor1 = 6; //
     public static final int turnEncoderChannel1 = 1; // placeholder
-
-    public static final int driveMotor2 = 7; // placeholder
-    public static final int turnMotor2 = 8; // placeholder
+//front right
+    public static final int driveMotor2 = 7; //
+    public static final int turnMotor2 = 8; //
     public static final int turnEncoderChannel2 = 2; // placeholder
-
-    public static final int driveMotor3 = 9; // placeholder
-    public static final int turnMotor3 = 10; // placeholder
+//back left
+    public static final int driveMotor3 = 9; //
+    public static final int turnMotor3 = 10; //
     public static final int turnEncoderChannel3 = 3; // placeholder
-
-    public static final int driveMotor4 = 11; // placeholder
-    public static final int turnMotor4 = 12; // placeholder
+//back right
+    public static final int driveMotor4 = 11; //
+    public static final int turnMotor4 = 12; //
     public static final int turnEncoderChannel4 = 4; // placeholder
-
 
     //the interavals in which the motor loops
     public static final double pdsec = 0.02; // always the same I think
