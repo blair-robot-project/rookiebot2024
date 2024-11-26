5147e9e58e3a16c6365eb0f8cebff6690238b2d3
EdzmLeGoat <edemsetse@gmail.com>
2024-11-26 13:14:49 -0500

    arm sim finished EXCEPT FOR CONSTANTS

--- src/main/java/frc/robot/allConstants/armConstants.java
+++ src/main/java/frc/robot/allConstants/armConstants.java
@@ -4,26 +4,27 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 
 public class armConstants {
-    //motor id
+    //motor id UNDETERMINED
     public static final int armMotorIDa = 62;
     //motor id for sim
     public static final int armSimID=19;
-    //motor follower id
+    //motor follower id UNDETERMINED
     public static final int armMotorFollowerID = 2;
-    //motor kp, ki, kd
+    //motor kp, ki, kd UNDETERMINED
     public static final double armKP = 0.1, armKI = 0, armKD = 0;
-    //default desired arm value
+    //default desired arm value UNDETERMINED (UNITS?)
     public static final double armDefaultDesiredValue = 0.25;
-    //default base arm value
+    //default base arm value UNDETERMINED (UNITS?)
     public static final double armDefaultBaseValue = 0;
-    //arm angle at the top
+    //arm angle at the top UNDETERMINED (UNITS?)
     public static final double armTopPositionValue = 0.5;
-    //gear ratio
+    //gear ratio UNDETERMINED
     public static final double armGearRatio = 1;
-    //desired arm value
+    //desired arm value UNDETERMINED (UNITS?)
     public static final double armDesiredValue = 0.25;
-    //base arm value
+    //base arm value UNDETERMINED (UNITS?)
     public static final double armBaseValue = 0;
+
 
     public static final double armFeedForwardKs=1;
     public static final double armFeedForwardKv=1;
@@ -33,7 +34,7 @@ public class armConstants {
     //VALUES NEEDED FOR ARM SIM
     //ALL VALUES BELOW ARE FAKE AND TEMPORARY
 
-    //arm dcmotor for arm sim FINALIZED
+    //arm dcmotor for arm sim FINALIZED I THINK?
     public static DCMotor armGearbox = DCMotor.getNEO(1);
     //gearing UNFINALIZED
     public static double armGearing = 1;
@@ -46,7 +47,7 @@ public class armConstants {
     public static double minAngleRads = Units.degreesToRadians(-60);
     //maxAngle (rads) UNFINALIZED
     public static double maxAngleRads = Units.degreesToRadians(120);
-    //whether or not to simualte gravity UNFINALIZED
+    //whether or not to simualte gravity FINALIZED
     public static boolean armSimGrav = true;
 
     //Encoder Channels UNFINALIZED
@@ -54,7 +55,8 @@ public class armConstants {
     public static int encoderBChannel = 8;
 
     // distance per pulse = (angle per revolution) / (pulses per revolution)
-    //  = (2 * PI rads) / (4096 pulses) UNFINALIZED
+    //  = (2 * PI rads) / (4096 pulses)
+    // UNFINALIZED
     public static double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096;
     public static final String kArmPositionKey = "ArmPosition";
     public static final String kArmPKey = "ArmP";
