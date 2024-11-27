ce8c17eb2bb8519a1c9632eed578a88d8cb2f47b
Ryanio2 <ryanxi20854@gmail.com>
2024-11-27 18:37:09 -0500

    removed unnecessary code when getting the swerve module position.

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -55,7 +55,7 @@ public class SwerveModule {
 
     public SwerveModulePosition getPosition() {
         return new SwerveModulePosition(
-                driveEncoder.getPosition() * WHEEL_CIRCUMFERENCE * driveGearing, new Rotation2d((turnEncoder.getAbsolutePosition() - turnEncoder.getPositionOffset()) / (2 * Math.PI)));
+                driveEncoder.getPosition(), new Rotation2d((turnEncoder.getAbsolutePosition() - turnEncoder.getPositionOffset()) / (2 * Math.PI)));
     }
 
     public SwerveModuleState getState() {
