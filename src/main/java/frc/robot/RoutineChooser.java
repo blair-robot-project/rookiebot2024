263ed703c2a2ae2882bdad14807ac107559a7b92
Ryanio2 <ryanxi20854@gmail.com>
2024-12-02 16:56:06 -0500

    Did some MORE work on the routine chooser.

--- src/main/java/frc/robot/RoutineChooser.java
+++ src/main/java/frc/robot/RoutineChooser.java
@@ -2,20 +2,47 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.autos.bottomPathClass;
+import frc.robot.autos.middlePathClass;
+import frc.robot.autos.taxiPathClass;
 
 public class RoutineChooser {
     private static final String bottomPathClass = "bottomPath"; // replace fillers with filenames of the autos, the strings are the assigned auto names
     private static final String middlePathClass = "middlePath";
     private static final String taxiPathClass = "taxiPath";
+    bottomPathClass bottomPath = new bottomPathClass();
+    middlePathClass middlePath = new middlePathClass();
+    taxiPathClass taxiPath = new taxiPathClass();
     private String m_autoSelected;
     private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+    // This should be called in the robot initialization to initialize autos.
     public void InitializeAutos() {
         m_chooser.setDefaultOption("filler1", taxiPathClass);
         m_chooser.addOption("middlePath", middlePathClass);
         m_chooser.addOption("bottom", bottomPathClass);
-        m_chooser.addOption("taxiPath", taxiPathClass);
         SmartDashboard.putData("Auto Choices", m_chooser);
     }
+
+    // Called in the initialization of the autos on the robots.
+    public void autonomousAutoInit() {
+        m_autoSelected = m_chooser.getSelected();
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case bottomPathClass:
+                bottomPath.bottomPath();
+                break;
+            case middlePathClass:
+                middlePath.middlePath();
+                break;
+            case taxiPathClass:
+            default:
+                taxiPath.taxiPath();
+                break;
+        }
+    }
 }
+
 
