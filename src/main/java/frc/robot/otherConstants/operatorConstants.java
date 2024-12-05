65c0bdc7fd39a73e2d3e5236ee29b50c8355cdda
joelamare <joelamare71@gmail.com>
2024-12-05 15:33:48 -0500

    controller ports

--- src/main/java/frc/robot/otherConstants/operatorConstants.java
+++ src/main/java/frc/robot/otherConstants/operatorConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.otherConstants;
 
 public class operatorConstants {
-    public static final int DRIVE_CONTROLLER_PORT = 0;//notional
+    public static final int DRIVE_CONTROLLER_PORT = 1;//notional
     public static final int MECH_CONTROLLER_PORT = 0; // notional
 }
