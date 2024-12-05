28b9ab44afcad1ea3a9f10dbf556ca52aca9af43
joelamare <joelamare71@gmail.com>
2024-12-05 15:35:15 -0500

    controller ports

--- src/main/java/frc/robot/subsystems/claw/clawConstants.java
+++ src/main/java/frc/robot/subsystems/claw/clawConstants.java
@@ -4,9 +4,9 @@ public class clawConstants {
     // used in claw subsystem
     public static final int CLAW_MOTOR_ID = 3; //notional value
     public static final int CLAW_FOLLOWER_ID = 4; //notional value
-    public static final double CLAW_INTAKE_VOLTAGE = 0.2; //notional
-    public static final double CLAW_OUTTAKE_VOLTAGE = -0.2; //notional
-    public static final double CLAW_HOLD_VOLTAGE = 0.0;//notional
+    public static final double CLAW_INTAKE_VOLTAGE = 5; //notional
+    public static final double CLAW_OUTTAKE_VOLTAGE = -3; //notional
+    public static final double CLAW_HOLD_VOLTAGE = 2;//notional
     public static final double CLAW_GEARING = 0.0; // notional
     public static final int CLAW_JKG = 0; // notional
     public static final double INTAKE_SECONDS=1;
